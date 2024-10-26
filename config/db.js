4b1783f78b9d09c8bb91dfb5ddc74f14252880d6
Md Shamsher Alam <145317544+MdShamsherAlam@users.noreply.github.com>
2024-10-26 12:23:11 +0530

    bugfix

--- config/db.js
+++ config/db.js
@@ -5,31 +5,32 @@ const cls = createNamespace("transaction-namespace"); // any string
 Sequelize.useCLS(cls);
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialectOptions: {
-            multipleStatements: true,
-            decimalNumbers: true
-        },
-        dialect: "mysql",
-        timezone: "+05:30",
-        host: process.env.DB_HOST,
-        define: {
-            //prevent sequelize from pluralizing table names
-            freezeTableName: true,
-        },
-        logging: false
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 4000, // Default TiDB port
+    dialect: "mysql",
+    dialectOptions: {
+      multipleStatements: true,
+      decimalNumbers: true,
+      ssl: {
+        rejectUnauthorized: true, // Ensures the certificate is validated
+      },
     },
-    {
-        pool: {
-            max: 1000,
-            min: 0,
-            acquire: 30000,
-            idle: 10000,
-        },
-    }
+    timezone: "+05:30",
+    define: {
+      freezeTableName: true, // Prevent table names from being pluralized
+    },
+    logging: false,
+    pool: {
+      max: 1000,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
 );
 
 module.exports = sequelize;
